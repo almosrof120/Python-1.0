824aec32e14ac02663def8f8e7690bd33b7edbe9
Al Mosrof Mia <65063391+almosrof120@users.noreply.github.com>
2023-02-09 09:50:49 -0800

    Update 14.py
    user input two int numbers for multiplication

--- 14.py
+++ 14.py
@@ -4,4 +4,7 @@ Ask user for quantity
 Suppose, one unit will cost 100.
 Judge and print total cost for user.
 """
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
 
+print(a*b)

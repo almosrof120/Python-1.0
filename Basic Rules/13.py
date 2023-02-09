7f9a0a5f76628ff08697c90e5555b0be1d0673b6
Al Mosrof Mia <65063391+almosrof120@users.noreply.github.com>
2023-02-09 11:37:33 -0800

    Create 13.py

---
+++ Basic Rules/13.py
@@ -0,0 +1,10 @@
+"""
+A shop will give discount of 10% if the cost of purchased quantity is more than 1000.
+Ask user for quantity
+Suppose, one unit will cost 100.
+Judge and print total cost for user.
+"""
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(a*b)

39727c7a959ec7667062e3c392375dc8bea41ff5
Al Mosrof Mia <65063391+almosrof120@users.noreply.github.com>
2023-02-09 11:35:23 -0800

    Create 09.py

---
+++ Basic Rules/09.py
@@ -0,0 +1,10 @@
+print("Enter the value of a:")
+a = input()
+print("Enter the value of b:")
+b = input()
+if a > b:
+    print("A is greatest number")
+elif a < b:
+    print("B is greatest number")
+else:
+    print("A & B is equal number")

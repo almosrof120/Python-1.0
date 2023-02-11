a999e3304ea26995c6e5474768c6b363e8bd9a8c
Al Mosrof Mia <65063391+almosrof120@users.noreply.github.com>
2023-02-10 23:32:22 -0800

    Create problem_3.py

---
+++ Basic problem solving/problem_3.py
@@ -0,0 +1,7 @@
+# Write a program to check whether a number entered by user is even or odd.
+
+Num = int (input("Enter a number: "))
+if Num % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")

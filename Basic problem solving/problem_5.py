3edc78db0e786972bb0b1137ec4ed643a14a81dd
Al Mosrof Mia <65063391+almosrof120@users.noreply.github.com>
2023-02-11 22:37:15 -0800

    Create problem_5.py

---
+++ Basic problem solving/problem_5.py
@@ -0,0 +1,7 @@
+# Write a program to display "Hello" if a number entered by user is a multiple of five, otherwise print "Bye".
+
+Num = int (input("Enter a number: "))
+if Num % 5 == 0:
+    print("Hello World!")
+else:
+    print("Bye!")

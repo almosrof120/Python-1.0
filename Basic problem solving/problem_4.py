b9d084edd23c8f9ff4424d8fd6fdcb002a9330b6
Al Mosrof Mia <65063391+almosrof120@users.noreply.github.com>
2023-02-11 22:35:39 -0800

    Create problem_4.py

---
+++ Basic problem solving/problem_4.py
@@ -0,0 +1,7 @@
+# Write a program to check whether a number is divisible by 7 or not.
+
+Num = int(input("Enter a number: "))
+if Num % 7 == 0:
+    print("This number is divisible by 7.")
+else:
+    print("This number is not divisible by 7.")

71fb4199deded93f239dff9be6d301d06bccaa31
Al Mosrof Mia <65063391+almosrof120@users.noreply.github.com>
2023-02-13 10:31:37 -0800

    Update problem_6.py

--- Basic problem solving/problem_6.py
+++ Basic problem solving/problem_6.py
@@ -7,3 +7,15 @@ Next 100 units          Rs 5 per unit
 After 200 units         Rs 10 per unit
 (For example if input unit is 350 than total bill amount is Rs2000)
 '''
+
+Bill = 0
+Unit = int (input("Enter the units of electricity: "))
+if Unit <= 100:
+    print("No Charge.")
+if 100 < Unit <= 200:
+    Bill = (Unit - 100) *5
+    print("The Bill amount is Rs", Bill)
+if Unit > 200:
+    Bill = 500 + (Unit - 200) *10
+    print("The bill amount is Rs", Bill)
+

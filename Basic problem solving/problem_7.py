755be178084d031a94db78f2f1a79bfa99556370
Al Mosrof Mia <65063391+almosrof120@users.noreply.github.com>
2023-02-13 11:56:03 -0800

    Update problem_7.py

--- Basic problem solving/problem_7.py
+++ Basic problem solving/problem_7.py
@@ -1,4 +1,8 @@
 """
 # Write a program to display the last digit of a number. (hint: any number % 10 will return the last digit)
 """
+num = int(input("Enter the number is: "))
+print("The last digit is",num%10)
+
+
 

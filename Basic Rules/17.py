68f618cd1db5266d44be6cb1806606d5fb8f9c3e
Al Mosrof Mia <65063391+almosrof120@users.noreply.github.com>
2023-02-24 07:22:37 -0800

    while loop

--- Basic Rules/17.py
+++ Basic Rules/17.py
@@ -1,1 +1,14 @@
+'''
+print all odd numbers from 1 to 100
+i = 1
+while i <= 100:
+    print(i)
+    i = i + 2
+print("end program")
+'''
 
+# print all even numbers from 1 to 100
+i = 2
+while i <= 100:
+    print(i)
+    i = i + 2

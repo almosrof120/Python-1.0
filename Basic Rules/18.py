1d599a99dd4dad32e16231f96848cf83072fa4c4
Al Mosrof Mia <65063391+almosrof120@users.noreply.github.com>
2023-02-24 11:20:15 -0800

    2+4+6....+n

---
+++ Basic Rules/18.py
@@ -0,0 +1,17 @@
+# print the sum from 1 to 100
+'''
+i = 1
+sum = 0
+while i <= 100:
+    sum = sum + i
+    i = i + 1
+print(sum)
+'''
+# for n
+n = int(input("Enter the value of n:"))
+sum = 0
+i = 1
+while i <= n:
+    sum = sum + i
+    i = i + 1
+print(sum)

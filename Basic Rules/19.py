dfb60580702917a4b711ea836ae543dffb951e61
Al Mosrof Mia <65063391+almosrof120@users.noreply.github.com>
2023-02-26 09:45:54 -0800

    break continue statements

---
+++ Basic Rules/19.py
@@ -0,0 +1,26 @@
+'''
+n = int(input("Enter the value of n:"))
+i = 2
+while n <= 100:
+    if i == 30:
+        break
+    print(i)
+    i = i + 2
+
+n = int(input("Enter the value of n: "))
+i = 1
+while n <= 50:
+    if i == 20:
+        continue
+    print(i)
+    i = i + 1
+print("Bangladesh")
+
+'''
+i = 1
+while i <= 100:
+    if i == 50:
+        continue
+    print(i)
+    i = i + 1
+print("Stop program!")
